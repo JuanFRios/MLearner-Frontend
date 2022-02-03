1617e5b7aeaec43430f084fc733652a5b54d28d8
paolaPosada <paola.posada1@udea.edu.co>
2022-02-03 15:52:54 -0500

    Contenido con props, falta conectar a la API

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,65 +1,59 @@
+/* eslint-disable no-lone-blocks */
 import React from 'react';
 
-export const Content = () => (
-  <div className=''>
-    <p className='text-2xl font-bold '>Contenido del curso</p>
-    <div className='flex flex-col justify-around items-center mt-8 text-white'>
-      <div className='moduleLg enable flex'>
-        <div className='flex flex-col self-baseline mt-4 pt-4 ml-4 pl-5'>
-          <p className='text-3xl font-bold'>32/32</p>
-          <div className='mt-16'>
-            <p className='text-2xl font-bold'>Introducción a Python</p>
+export const Content = ({
+  nombre,
+  puntajeObtenido,
+  puntajetotal,
+  tamaño,
+  activo,
+}) => {
+  let icono;
+  let color;
+  let aux;
+
+  if (activo === 'False') {
+    icono = 'bx:bxs-lock';
+    color = 'locked';
+  } else {
+    icono = 'iconoir:gym';
+    color = 'enable';
+  }
+
+  if (tamaño === 'moduleLg') {
+    aux = (
+      <div className='flex flex-col items-center my-2 text-white'>
+        <div className={`${tamaño} ${color} flex m-2 p-4`}>
+          <div className='flex flex-col self-baseline'>
+            <p className='text-3xl font-bold'>{`${puntajeObtenido}/${puntajetotal}`}</p>
+            <div className='mt-20 w-64'>
+              <p className='text-2xl font-bold'>{nombre}</p>
+            </div>
+          </div>
+          <div className='flex justify-items-end ml-72 pl-72 items-end'>
+            <span className='iconify text-5xl' data-icon={icono} />
           </div>
         </div>
-        <div className='flex items-center mt-28 ml-52 pl-52'>
-          <span className='iconify text-5xl ' data-icon='iconoir:gym' />
-        </div>
       </div>
-    </div>
-    <div className='flex'>
-      <div className='flex flex-col justify-around mt-8 ml-40 text-white'>
-        <div className='moduleSm bad flex'>
-          <div className='flex flex-col self-baseline my-2 py-4 ml-4 pl-5'>
-            <p className='text-3xl font-bold '>10/32</p>
+    );
+  } else {
+    aux = (
+      <div className='flex flex-col my-8 text-white'>
+        <div className={`${tamaño} ${color} flex p-4`}>
+          <div className='flex flex-col self-baseline'>
+            <p className='text-3xl font-bold'>{`${puntajeObtenido}/${puntajetotal}`}</p>
             <div className='mt-12 flex'>
               <p className='w-40 text-2xl font-bold justify-items-center'>
-                Ingeniería de Características
+                {nombre}
               </p>
               <div className='items-center ml-28 pt-4'>
-                <span className='iconify text-5xl' data-icon='whh:repeat' />
+                <span className='iconify text-5xl' data-icon={icono} />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='flex flex-col justify-around mt-8 ml-9 text-white'>
-        <div className='moduleSm locked flex'>
-          <div className='flex flex-col self-baseline my-2 py-4 ml-4 pl-5'>
-            <p className='text-3xl font-bold '>0/32</p>
-            <div className='mt-12 flex'>
-              <p className='w-44 text-2xl font-bold justify-items-center'>
-                Reducción de dimensionalidad
-              </p>
-              <div className='items-center ml-20 pt-4'>
-                <span className='iconify text-5xl ' data-icon='bx:bxs-lock' />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='flex flex-col justify-around items-center mt-8 text-white'>
-      <div className='moduleLg locked flex'>
-        <div className='flex flex-col self-baseline mt-4 pt-4 ml-4 pl-5'>
-          <p className='text-3xl font-bold'>0/32</p>
-          <div className='mt-16'>
-            <p className='text-2xl font-bold'>Introducción a Python</p>
-          </div>
-        </div>
-        <div className='flex items-center mt-28 ml-52 pl-52'>
-          <span className='iconify text-5xl ' data-icon='bx:bxs-lock' />
-        </div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+  return <div>{aux}</div>;
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,39 +4,71 @@ import InformationProgress from 'components/InformationProgress';
 import React from 'react';
 
 const Dashboard = () => (
-  <div className='flex flex-col ml-64 pl-28 pr-4 pt-5'>
-    <div className='flex justify-end mr-6'>
-      <InformationProgress
-        className='basis-1/4'
-        icono='grommet-icons:diamond'
-        color='text-sky-400'
-        titulo='Puntaje'
-        informacion='10/30'
-      />
-      <InformationProgress
-        className='basis-1/4'
-        icono='ic:sharp-local-fire-department'
-        color='text-orange-600'
-        titulo='Racha'
-        informacion='7'
-      />
-      <InformationProgress
-        className='basis-1/4'
-        icono='mdi:progress-clock'
-        color='text-green-600'
-        titulo='Progreso'
-        informacion='10%'
+  <div className='ml-64 pl-28 pr-4 pt-5'>
+    <div className='flex flex-col'>
+      <div className='flex justify-end mr-6'>
+        <InformationProgress
+          className='basis-1/4'
+          icono='grommet-icons:diamond'
+          color='text-sky-400'
+          titulo='Puntaje'
+          informacion='10/30'
+        />
+        <InformationProgress
+          className='basis-1/4'
+          icono='ic:sharp-local-fire-department'
+          color='text-orange-600'
+          titulo='Racha'
+          informacion='7'
+        />
+        <InformationProgress
+          className='basis-1/4'
+          icono='mdi:progress-clock'
+          color='text-green-600'
+          titulo='Progreso'
+          informacion='10%'
+        />
+      </div>
+      <div>
+        <p className='text-2xl font-bold'>Hola Felipe!, sigue aprendiendo</p>
+      </div>
+      <ContinueLesson
+        leccion='Lección: Procesamiento con pandas'
+        modulo='Modulo: Ingeniería de características'
       />
     </div>
-    <div>
-      <p className='text-2xl font-bold'>Hola Felipe!, sigue aprendiendo</p>
+    <div className='my-6 pt-3 text-2xl font-bold'>
+      <p>Contenido del curso</p>
     </div>
-    <ContinueLesson
-      leccion='Lección: Procesamiento con pandas'
-      modulo='Modulo: Ingeniería de características'
-    />
-    <div className='my-16'>
-      <Content />
+    <div className='containerContent flex flex-wrap justify-between my-4'>
+      <Content
+        nombre='Introducción a Python'
+        puntajeObtenido='32'
+        puntajetotal='32'
+        tamaño='moduleLg'
+        activo='True'
+      />
+      <Content
+        nombre='Ingeniería de Características'
+        puntajeObtenido='0'
+        puntajetotal='32'
+        tamaño='moduleSm'
+        activo='False'
+      />
+      <Content
+        nombre='Reducción de dimensionalidad'
+        puntajeObtenido='0'
+        puntajetotal='32'
+        tamaño='moduleSm'
+        activo='False'
+      />
+      <Content
+        nombre='Clustering'
+        puntajeObtenido='0'
+        puntajetotal='32'
+        tamaño='moduleLg'
+        activo='False'
+      />
     </div>
   </div>
 );
